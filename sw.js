61c1484c209c38177814de51991e4cb4d296e7b8
minhankim <66061169+minhankim@users.noreply.github.com>
2026-03-06 16:20:18 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+var CACHE_NAME = 'blackjack-v1';
+var urlsToCache = [
+  './',
+  './index.html',
+  './favicon.ico',
+  './icon-192.png',
+  './icon-512.png',
+  './1번.mp3',
+  './2번.mp3',
+  './딜러.jpg'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; })
+          .map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(res) {
+        if (res.status === 200) {
+          var clone = res.clone();
+          caches.open(CACHE_NAME).then(function(cache) { cache.put(e.request, clone); });
+        }
+        return res;
+      });
+    }).catch(function() {
+      return caches.match('./index.html');
+    })
+  );
+});
